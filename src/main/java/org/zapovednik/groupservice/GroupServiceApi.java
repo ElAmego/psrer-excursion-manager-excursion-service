0b0f6cbd64c7b9b27194bd19908b8774235bfe1a
Egor <el.amegoal@gmail.com>
2026-06-30 12:00:57 +0300

    chore: initial setup for group-service api
    - Change packaging from war to jar
    - Remove JUnit dependency
    - Add spring-boot-starter-web dependency (Spring Boot 4.1.0-RC1)
    - Add GroupServiceApi entry point with @SpringBootApplication
    - Update .gitignore: replace specific .idea files with .idea/ directory

---
+++ src/main/java/org/zapovednik/groupservice/GroupServiceApi.java
@@ -0,0 +1,11 @@
+package org.zapovednik.groupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupServiceApi {
+    public static void main(String[] args) {
+        SpringApplication.run(GroupServiceApi.class, args);
+    }
+}
